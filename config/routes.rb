76818c2d747fb231125e58dfcc213df23398c1b6
Ykarz <blhlc0820@gmail.com>
2025-04-20 15:35:10 +0900

    [add]Userモデルの作成、deviseのカスタマイズ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get "static/top"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -12,4 +11,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "static#top"
+
+  # 'devise'の各種コントローラへのルーティング
+  devise_for :users,
+            path: "",
+            path_names: { sign_in: 'login', sign_out: 'logout' }
 end
