01707063c403fab9ae921c98dbf71ce7c71e8242
Ykarz <blhlc0820@gmail.com>
2025-05-05 01:45:25 +0900

    [add]countryモデル、countriesテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_05_04_154147) do
+ActiveRecord::Schema[7.2].define(version: 2025_05_04_164114) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "countries", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "stores", force: :cascade do |t|
     t.string "place_id", null: false
