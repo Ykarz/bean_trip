b2e3f2844949b53cced10ba3e8ec42ed4718d119
Ykarz <blhlc0820@gmail.com>
2025-05-24 17:50:57 +0900

    [add]indexアクションに検索処理を追加

--- app/models/country.rb
+++ app/models/country.rb
@@ -2,4 +2,9 @@ class Country < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length: { maximum: 255 }
 
   has_many :beans
+
+  # Ransackが検索可能なカラムを設定
+  def self.ransackable_attributes(auth_object = nil)
+    ["name"]
+  end
 end
