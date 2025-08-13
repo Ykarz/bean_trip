f29c69761b68e0e7eefca5bc61150bb74f2c205e
Ykarz <blhlc0820@gmail.com>
2025-08-13 14:02:21 +0900

    [update]photo_referenceを取得するメソッドを追加

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -5,5 +5,34 @@ class StoresController < ApplicationController
 
   def show
     @store = Store.includes(:beans).find(params[:id])
+    @photo_reference = get_place_photo_reference(@store.place_id)
   end
 end
+
+  private
+
+  # 店舗の写真を表示するためにAPIを叩いて、'photo_reference'を取得するメソッド
+  def get_place_photo_reference(place_id)
+    require 'open-uri'
+    require 'json'
+
+    # クエリパラメータの作成
+    request_query = URI.encode_www_form(
+      place_id: place_id,
+      language: 'ja',
+      key: ENV['GOOGLE_MAPS_API_KEY']
+    )
+
+    # リクエスト用のURLを生成
+    request_url = "https://maps.googleapis.com/maps/api/place/details/json?#{request_query}"
+    # APIを叩いてJSON形式でデータを取得
+    place_detail_json_data = URI.open(request_url).read
+    # JSON形式のデータをRubyオブジェクトに変換
+    place_detail_data = JSON.parse(place_detail_json_data)
+
+    # 取得した場所データから'photo_reference'を抽出
+    photo_reference = place_detail_data['result']['photos']&.first&.dig('photo_reference')
+
+    # 取得した'photo_reference'を返す
+    photo_reference
+  end
