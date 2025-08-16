2b1bcca9e2d1873e0e2149fd2f74f8114054e105
Ykarz <blhlc0820@gmail.com>
2025-08-16 14:57:38 +0900

    [add]Ransackが検索可能なカラムを設定

--- app/models/store.rb
+++ app/models/store.rb
@@ -2,4 +2,9 @@ class Store < ApplicationRecord
   validates :place_id, :name, :latitude, :longitude, presence: true, uniqueness: true
 
   has_many :beans
+
+  # Ransackが検索可能なカラムを設定
+  def self.ransackable_attributes(auth_object = nil)
+    ["name", "address"]
+  end
 end
