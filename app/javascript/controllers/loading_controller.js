a0ecae37b1e969631516fd524623ae4f3e9232ab
Ykarz <blhlc0820@gmail.com>
2026-01-03 00:22:01 +0900

    [add]ローディングアニメーションをレイアウトに追加し、表示用のロジックをコントローラーに追記

--- app/javascript/controllers/loading_controller.js
+++ app/javascript/controllers/loading_controller.js
@@ -2,6 +2,20 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="loading"
 export default class extends Controller {
+  // loadingコントローラーのターゲット(DOM要素)を定義
+  static targets = [ "spinner" ]
+
+  // コントローラーが接続されたときに実行されるメソッド
   connect() {
+    this.hide()
+  }
+
+  // ローディングアニメーションを表示するメソッド
+  show() {
+    this.spinnerTarget.classList.remove("hidden")
+}
+  // ローディングアニメーションを非表示にするメソッド
+  hide() {
+    this.spinnerTarget.classList.add("hidden")
   }
 }
