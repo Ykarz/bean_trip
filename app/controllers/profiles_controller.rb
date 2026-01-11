c188ae7099108bdd3dec54818dba5c1271f634aa
Ykarz <blhlc0820@gmail.com>
2026-01-12 00:44:05 +0900

    [add]プロフィール編集ページと更新機能の実装

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,8 +2,29 @@ class ProfilesController < ApplicationController
   def show; end
 
   def edit
+    # 現在ログインしているユーザーの情報を取得し、@userに代入
+    @user = User.find(current_user.id)
   end
 
   def update
+    # 現在ログインしているユーザーの情報を取得し、@userに代入
+    @user = User.find(current_user.id)
+
+    # user_params（ストロングパラメータ）で@userを更新
+    if @user.update(user_params)
+      # 更新に成功した場合、プロフィール表示ページへリダイレクトし、成功メッセージを表示
+      redirect_to profile_path, notice: t('profiles.update.success')
+    else
+      # 更新に失敗した場合、編集フォームを再表示し、エラーメッセージを表示
+      flash.now[:alert] = t('profiles.update.failure')
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  # ストロングパラメータの設定
+  def user_params
+    params.require(:user).permit(:name, :email, :avatar, :avatar_cache)
   end
 end
