b46de88c22b08d966fcdf944d93c1a24f4bdd619
Ykarz <blhlc0820@gmail.com>
2026-01-20 18:15:11 +0900

    [update]ユーザーが同じ店舗を複数回ブックマークできないようにバリデーションを追加

--- app/models/store_bookmark.rb
+++ app/models/store_bookmark.rb
@@ -1,4 +1,7 @@
 class StoreBookmark < ApplicationRecord
   belongs_to :user
   belongs_to :store
+
+  # 'user_id'と'store_id'の組み合わせが一意であることを保証するバリデーション
+  validates :user_id, uniqueness: { scope: :store_id }
 end
